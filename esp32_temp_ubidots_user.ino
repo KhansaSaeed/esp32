3a10f24cf8028fb4029eb7340b697d13e4b0d3dc
Khansa <42270930+KhansaSaeed@users.noreply.github.com>
2018-12-22 02:12:53 +0500

    Add files via upload

---
+++ esp32_temp_ubidots_user.ino
@@ -0,0 +1,128 @@
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define WIFISSID "your wifi name" // Put your WifiSSID here
+#define PASSWORD "your wifi password" // Put your wifi password here
+#define TOKEN "your token no" // Put your Ubidots' TOKEN
+#define MQTT_CLIENT_NAME "4fafc201" // MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string; 
+                                           //it should be a random and unique ascii string and different from all other devices
+
+#define VARIABLE_LABEL "TempSensor" // Assing the variable label
+#define DEVICE_LABEL "esp32" // Assig the device label
+
+char mqttBroker[]  = "things.ubidots.com";
+char payload[100];
+char topic[150];
+// Space to store values to send
+char str_sensor[10];
+char str_lat[6];
+char str_lng[6];
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+WiFiClient ubidots;
+PubSubClient client(ubidots);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  String message(p);
+  Serial.write(payload, length);
+  Serial.println(topic);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    
+    // Attemp to connect
+    if (client.connect(MQTT_CLIENT_NAME, TOKEN, "")) {
+      Serial.println("Connected");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 2 seconds");
+      // Wait 2 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFISSID, PASSWORD);
+  // Assign the pin as INPUT 
+
+  Serial.println();
+  Serial.print("Wait for WiFi...");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqttBroker, 1883);
+  client.setCallback(callback);  
+}
+
+void loop() {
+
+  Serial.print("Temperature: ");
+  
+  // Convert raw temperature in F to Celsius degrees
+  Serial.print((temprature_sens_read() - 32) / 1.8);
+  Serial.println(" C");
+  
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+  sprintf(payload, "%s", ""); // Cleans the payload
+  sprintf(payload, "{\"%s\":", VARIABLE_LABEL); // Adds the variable label
+  
+  float sensor = (temprature_sens_read() - 32) / 1.8;
+  
+  float lat = 6.101;
+  float lng= -1.293;
+
+  /* 4 is mininum width, 2 is precision; float value is copied onto str_sensor*/
+  dtostrf(sensor, 4, 2, str_sensor);
+  dtostrf(lat, 4, 2, str_lat);
+  dtostrf(lng, 4, 2, str_lng);  
+  
+  sprintf(payload, "%s {\"value\": %s", payload, str_sensor); // Adds the value
+ sprintf(payload, "%s, \"context\":{\"lat\": %s, \"lng\": %s}", payload, str_lat, str_lng); // Adds coordinates
+  sprintf(payload, "%s } }", payload); // Closes the dictionary brackets
+  Serial.println("Publishing data to Ubidots Cloud");
+  client.publish(topic, payload);
+  client.loop();
+  delay(5000);
+  
+}
